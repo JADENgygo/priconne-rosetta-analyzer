522bfb4edbbea83cfc5ec5171d6d92137010ce37
JADEN <29948746+JADENgygo@users.noreply.github.com>
2022-09-23 21:25:31 +0900

    fix

--- priconne-rosetta-analyzer/Program.cs
+++ priconne-rosetta-analyzer/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using Vosk;
+using NAudio.Wave;
 
 namespace priconne_rosetta_analyzer
 {
@@ -6,7 +9,40 @@ namespace priconne_rosetta_analyzer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string modelPath = "model";
+            const int sampleRate = 44100;
+            //const int sampleRate = 16000;
+
+            if (!Directory.Exists(modelPath)) {
+                Console.WriteLine("here");
+                return;
+            }
+
+            Vosk.Vosk.SetLogLevel(0);
+            var model = new Model(modelPath);
+            var rec = new VoskRecognizer(model, sampleRate);
+
+            var waveIn = new WaveInEvent();
+            const int deviceNumber = 0;
+            waveIn.DeviceNumber = deviceNumber;
+            waveIn.WaveFormat = new WaveFormat(sampleRate, WaveIn.GetCapabilities(deviceNumber).Channels);
+            waveIn.DataAvailable += (_, e) =>
+            {
+                if (rec.AcceptWaveform(e.Buffer, e.BytesRecorded))
+                {
+                    Console.WriteLine(rec.Result());
+                }
+                else
+                {
+                    Console.WriteLine(rec.PartialResult());
+                }
+            };
+            waveIn.StartRecording();
+            while (true)
+            {
+                ;
+            }
         }
     }
 }
+
